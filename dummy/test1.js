6ced8fc0542ea5f1d919a2298d5e478f737855c1
Aniketh <aniketh317@gmail.com>
2022-07-07 00:49:40 +0530

    Some playing arnd

--- dummy/test1.js
+++ dummy/test1.js
@@ -1,2 +1,22 @@
-const names = "My name is Aniketh"
+var names = "My name is Aniketh"
 console.log(`${names}`)
+//alert(names)
+names  = "Changed the value"
+console.log(`${names}`)
+//alert(names)
+let x
+let a = 2
+let b = 3
+x = a**b
+console.log(`${x}`)
+
+console.log("STart with Objects");
+var me = new Object()
+me.s = 44
+console.log(me.s);
+me = {
+    s1:11,
+    s2:22
+};
+console.log(me.s);
+console.log(me.s2);
---
+++ dummy/test2.js
@@ -0,0 +1,10 @@
+let a = new Number(50)
+let b = a
+let c = new Number(50)
+console.log(a==b)
+console.log(a===b)
+console.log(a===c)
+console.log(a==c)
+console.log(typeof(a));
+a = 25;
+console.log(typeof(a))
